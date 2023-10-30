82a86d35bf6695e8e3941816dc4966e0aef1282e
KereneL <kerenel@gmail.com>
2023-10-30 17:50:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,260 @@
+export default function init() {
+  var activeEffect;
+  var activeLayer;
+  var divs = []
+
+  divs.push(...$("#bayer-effect, #gamma-effect, #channels-effect"))
+  function hideAllEffectDivs() {
+    divs.forEach(div => {
+      $(div).hide();
+    })
+  }
+
+  $("#toolbox-list-acc, #layers-list-acc").accordion({
+    collapsible: true,
+    classes: {
+      "ui-accordion-header": "",
+      "ui-accordion-header-collapsed": "",
+      "ui-corner-bottom": "",
+    },
+  })
+
+  $("#bayer-effect").data("default", { name: "bayer", displayName: "Bayer Matrix", n: 1, spread: 0.5 })
+  $("#bayer-effect").data("config", Object.assign({},$("#bayer-effect").data("default")))
+  $("#bayer-select").selectmenu({
+    create: function (event, ui) {
+      $(this).val("1");
+      $("#bayer-effect").data("config").n = $(this).val()
+      $(this).selectmenu("refresh");
+    },
+    change: function (event, ui) {
+      $("#bayer-effect").data("config").n = ui.item.value;
+      activeLayer.data("config", Object.assign({},$("#bayer-effect").data("config")))
+    },
+  })
+  $("#bayer-spread-slider").slider({
+    min: 0,
+    max: 4,
+    step: 0.1,
+    value: 0.5,
+    create: function () {
+      $("#bayer-spread-handle").text($(this).slider("value"));
+      $("#bayer-effect").data("config").spread = 0.5;
+    },
+    slide: function (event, ui) {
+      $("#bayer-spread-handle").text(ui.value);
+      $("#bayer-effect").data("config").spread = ui.value;
+      activeLayer.data("config", $("#bayer-effect").data("config"))
+    },
+  });
+  $("#bayer-effect").data("load", (configObj)=>{
+    $("#bayer-effect").data("config", configObj)
+    $("#bayer-select").val(configObj.n).selectmenu("refresh")
+    $("#bayer-spread-slider").slider({value: configObj.spread})
+    let left = (configObj.spread / $("#bayer-spread-slider").slider( "option", "max" ));
+    $("#bayer-spread-handle").css("left",`${left*100}%`)
+    $("#bayer-spread-handle").text(configObj.spread);
+  })
+
+
+  $("#gamma-effect").data("default", { name: "gamma", displayName: "Gamma", value: 1})
+  $("#gamma-effect").data("config", Object.assign({},$("#gamma-effect").data("default")))
+  $("#gamma-slider").slider({
+    min: 0,
+    max: 10,
+    step: 0.025,
+    value: 1,
+    create: function () {
+      $("#gamma-handle").text($(this).slider("value"));
+      $("#gamma-effect").data("config").value = 1;
+    },
+    slide: function (event, ui) {
+      $("#gamma-handle").text(ui.value);
+      $("#gamma-effect").data("config").value = ui.value;
+      activeLayer.data("config", Object.assign({},$("#gamma-effect").data("config")))
+    }
+  });
+  $("#gamma-effect").data("load", (configObj)=>{
+    $("#gamma-effect").data("config", configObj)
+    $("#gamma-slider").slider({value: configObj.value})
+    let left = (configObj.value / $("#gamma-slider").slider( "option", "max" ));
+    $("#gamma-handle").css("left",`${left*100}%`)
+    $("#gamma-handle").text(configObj.value)
+  })
+
+
+  $("#channels-effect").data("default", { "name": "channels", displayName: "Channels", "r": true, "g": true, "b": true, "gs": false })
+  $("#channels-effect").data("config", Object.assign({},$("#channels-effect").data("default")))
+
+  $("#channels-effect").data("load", (configObj)=>{
+    $("#channels-effect").data("config", configObj)
+    $("#checkbox-r").prop("checked", configObj.r).checkboxradio("refresh")
+    $("#checkbox-g").prop("checked", configObj.g).checkboxradio("refresh")
+    $("#checkbox-b").prop("checked", configObj.b).checkboxradio("refresh")
+    $("#checkbox-gs").prop("checked", configObj.gs).checkboxradio("refresh")
+  })
+  $("#channels-fs").controlgroup({
+    "direction": "vertical"
+  });
+  $("#checkbox-r").checkboxradio({ label: "Red" })
+    .prop("checked", true)
+    .checkboxradio("refresh");
+  $("#checkbox-g").checkboxradio({ label: "Green" })
+    .prop("checked", true)
+    .checkboxradio("refresh");
+  $("#checkbox-b").checkboxradio({ label: "Blue" })
+    .prop("checked", true)
+    .checkboxradio("refresh");
+  $("#checkbox-gs").checkboxradio({ label: "Grayscale" })
+    .prop("checked", false)
+    .checkboxradio("refresh");
+  function disableGrayscaleChannel() {
+    $("#checkbox-gs").prop("checked", false);
+    $("#checkbox-gs").checkboxradio("refresh")
+  }
+  $("#checkbox-r").change(function () {
+    if (this.checked) disableGrayscaleChannel()
+    $("#channels-effect").data("config").r = this.checked
+    activeLayer.data("config", Object.assign({},$("#channels-effect").data("config")))
+  })
+  $("#checkbox-g").change(function () {
+    if (this.checked) disableGrayscaleChannel()
+    $("#channels-effect").data("config").g = this.checked
+    activeLayer.data("config", Object.assign({},$("#channels-effect").data("config")))
+  });
+  $("#checkbox-b").change(function () {
+    if (this.checked) disableGrayscaleChannel()
+    $("#channels-effect").data("config").b = this.checked
+    activeLayer.data("config", Object.assign({},$("#channels-effect").data("config")))
+  });
+  $("#checkbox-gs").change(function () {
+    colChannels(!this.checked)
+    $("#channels-effect").data("config").gs = this.checked
+    activeLayer.data("config", Object.assign({},$("#channels-effect").data("config")))
+  })
+
+  $(document).on("click", "span.delete-handle", function (event, ui) {
+    event.preventDefault();
+    let toBeRemoved = $(this).closest("li")
+    
+    if (toBeRemoved.hasClass("ui-selected")) {
+      activeEffect = undefined;
+      activeLayer = undefined;
+      hideAllEffectDivs();
+    }
+      toBeRemoved.remove();
+      if ($("#layers-list").children(".ui-selected").length != 0){
+        let selectedTop = $("#layers-list").children(".ui-selected").eq(0)
+        let topValue =  0 + selectedTop.offset().top
+        activeEffect.css({"top": topValue})
+      }
+    }
+    );
+  $("#layers-list")
+    .sortable({
+      handle: ".drag-handle",
+      placeholder: "ui-sortable-placeholder ui-state-highlight layer-list-placeholder",
+      appendTo: $("main"),
+      opacity: 0.7,
+      helper: "original",
+      revert: 64,
+    })
+    .selectable({
+      filter: "li",
+      cancel: "span",
+      selected: function (event, ui) {
+        $(ui.selected).addClass("ui-selected").siblings().removeClass("ui-selected").each(
+          function (key, value) {
+            $(value).find("*").removeClass("ui-selected");
+          }
+        );
+        activeLayer = $(ui.selected)
+        let type = activeLayer.data("type")
+        activeEffect = $(`#${type}-effect`)
+        let positionRight = $("#toolbox").width()
+        let positionTop = activeLayer.offset().top
+        activeEffect.css({ "right": positionRight, "top": positionTop });
+        let data = activeLayer.data("config")
+        activeEffect.data("load")(data);
+        hideAllEffectDivs();
+        activeEffect.show();
+      },
+      unselected: function (event, ui) {
+        activeLayer = undefined;
+        activeEffect = undefined;
+        hideAllEffectDivs();
+      }
+    })
+
+  $("#effects").selectmenu({
+    change: function (event, ui) {
+      if (ui.item.value == "none") return;
+
+      hideAllEffectDivs();
+
+      let newItem = $(`
+          <li class="ui-state-default layer-row ui-selected">
+            <div class="layer-start">
+                <span class="drag-handle">&equiv;</span><label>${ui.item.label}</label>
+            </div>
+            <div class="layer-end">
+                <span class="delete-handle">&#10005;</span>
+            </div>
+          </li>
+          `)
+      newItem.data("type", ui.item.value)
+      $("#layers-list").append(newItem);
+      newItem.siblings(".ui-selected").removeClass("ui-selected")
+      let type = ui.item.value
+      
+      activeLayer = newItem
+      activeEffect = $(`#${type}-effect`)
+      activeLayer.data("config", Object.assign({}, activeEffect.data("default")));
+      let data = activeLayer.data("config")
+      activeEffect.data("load")(data)
+      $("#layers-list").scrollTop($("#layers-list")[0].scrollHeight);
+      let positionRight = $("#toolbox").width()
+      let positionTop = newItem.offset().top
+      activeEffect.css({ "right": positionRight, "top": positionTop });
+      $("#layers-list").selectable("refresh")
+      hideAllEffectDivs();
+      activeEffect.show();
+    },
+    close: function (event, ui) {
+      $(this).val("none")
+      $(this).selectmenu("refresh");
+    },
+    create: function (event, ui) {
+      $(this).val("none");
+      $(this).selectmenu("refresh");
+    }
+  });
+
+  let inputObj = p5Obj.createFileInput(handleFile);
+  $(inputObj.elt).attr("id", "file-upload");
+  $(inputObj.elt).hide();
+
+  $("#toolbox-list-acc, #layers-list-acc, ul, li").disableSelection();
+  hideAllEffectDivs();
+  p5Obj.redraw = redraw;
+}
+
+function redraw() {
+  const algo = [];
+  let children = $("#layers-list").children();
+  children.each(
+    function (index, element) {
+      let effectConfig = $(element).data("config")
+      if (!effectConfig) return;
+      else algo.push(effectConfig)
+    });
+  startAlgo(algo)
+}
+function colChannels(checked) {
+  $("#channels-effect").data("config").r = checked;
+  $("#channels-effect").data("config").g = checked;
+  $("#channels-effect").data("config").b = checked;
+  $("#checkbox-r").prop("checked", checked).checkboxradio("refresh");
+  $("#checkbox-g").prop("checked", checked).checkboxradio("refresh");
+  $("#checkbox-b").prop("checked", checked).checkboxradio("refresh");
+}
---
+++ sketch.js
@@ -0,0 +1,190 @@
+var img;
+var imgWidth;
+var imgHeight;
+var path = "./images/Mountain.jpg";
+
+var p5Obj = new p5(p5=>{
+  p5.originalImg = undefined;
+  p5.finalImg = undefined;
+  //p5.redraw = undefined;
+  p5.setup = function () {
+    p5.createCanvas(400, 400).imageSmoothingEnabled = false;
+    
+    p5.background(255);
+    p5.text("load an image to start", 12, 12, 372, 40)
+    p5.originalImg = p5Obj.loadImage(path, image=>{
+      loadNewImg(image.get())
+    });
+  }
+});
+
+function showOriginal() {
+  p5Obj.image(p5Obj.originalImg, 0, 0);
+}
+function showFinal() {
+  p5Obj.image(p5Obj.finalImg,0,0)
+}
+function handleFile(file) {
+  if (file.type === 'image') {
+    p5Obj.loadImage(file.data, loaded=>{
+      loadNewImg(loaded) 
+    })
+  }
+}
+function loadNewImg(loadedImage) {
+  p5Obj.originalImg = loadedImage.get()
+  img = p5Obj.originalImg.get()
+  imgWidth = img.width;
+  imgHeight = img.height;
+  p5Obj.resizeCanvas(imgWidth, imgHeight);
+};
+
+function startAlgo(algo) {
+  img = p5Obj.originalImg.get()
+  img.loadPixels()
+  algo.forEach((layer,index)=>{
+    switch(layer.name){
+      case "bayer":{
+        bayerDither(layer.n,0.5/*layer.spread*/);
+        break;
+      }
+      case "channels":{
+        channelsFilter(layer.r,layer.g,layer.b,layer.gs);
+
+        break;
+      }
+      case "gamma":{
+        gammaCorrect(layer.value)
+        break;
+      }
+      default:{
+        break;
+      }
+    }
+    
+  })
+  img.updatePixels()
+  p5Obj.finalImg = img.get()
+  showFinal();
+}
+
+function getPixel(index) {
+  return img.pixels[index]
+}
+function gammaCorrect(gamma){
+  for (let y = 0; y < imgHeight; y++) {
+    for (let x = 0; x < imgWidth; x++) {
+      const index = (x + y * imgWidth) * 4;
+  img.pixels[index + 0] = ((img.pixels[index + 0]/255) ** gamma)*255
+  img.pixels[index + 1] = ((img.pixels[index + 1]/255) ** gamma)*255
+  img.pixels[index + 2] = ((img.pixels[index + 2]/255) ** gamma)*255
+    }
+  }
+}
+function channelsFilter(r, g, b, gs) {
+  for (let y = 0; y < imgHeight; y++) {
+    for (let x = 0; x < imgWidth; x++) {
+      const index = (x + y * imgWidth) * 4;
+
+      let initR  = getPixel(index + 0);
+      let initG  = getPixel(index + 1);
+      let initB  = getPixel(index + 2);
+      let initGs = getRedLuminance(initR) + getGreenLuminance(initG) + getBlueLuminance(initB)
+
+      img.pixels[index + 0] = ((r) ? initR : ((gs) ? initGs : 0))
+      img.pixels[index + 1] = ((g) ? initG : ((gs) ? initGs : 0))
+      img.pixels[index + 2] = ((b) ? initB : ((gs) ? initGs : 0))
+    }
+  }
+}
+
+function bayerDither(dimensions, spread) {
+  let n = 2 ** dimensions;
+  let bayer = bayerMatrices[dimensions];
+  let normBayerM = bayer.map(val => {
+    return ((val) / (n ** 2)) -.05
+  })
+
+  for (let y = 0; y < imgHeight; y++) {
+    for (let x = 0; x < imgWidth; x++) {
+      const index = (x + y * imgWidth) * 4;
+
+      //פחות כפל, חילוק
+      let r, g, b
+      r = getPixel(index + 0)/ 255.0
+      g = getPixel(index + 1)/ 255.0
+      b = getPixel(index + 2)/ 255.0
+
+      const bayerY = y % n;
+      const bayerX = x % n;
+      const M = normBayerM[bayerX + bayerY * n];
+
+      function calcColorc(c){
+        c += spread * M;
+        c = Math.floor(c*(n-1)+0.5)/(n-1);
+        return c;
+      };
+      r=calcColorc(r)
+      g=calcColorc(g)
+      b=calcColorc(b)
+
+      let pixelR = Math.floor(255 * r)
+      let pixelG = Math.floor(255 * g)
+      let pixelB = Math.floor(255 * b)
+
+      img.pixels[index + 0] = pixelR;
+      img.pixels[index + 1] = pixelG;
+      img.pixels[index + 2] = pixelB;
+    }
+  }
+}
+
+function srgbToLinear(s) {
+  let l = (s <= 0.0404482362771082) ? (s / 12.92) : (((s + 0.055) / 1.055) ** gamma)
+  return Math.max(0, Math.min(l, 1));
+}
+function linearToSRGB(l) {
+  let s = (l <= 0.00313066844250063) ? (l * 12.92) : 1.055 * (l ** (1 / gamma)) - 0.055
+  return Math.max(0, Math.min(s, 1));
+}
+function getRedLuminance(r) {
+  return 0.2126 * r;
+}
+function getGreenLuminance(g) {
+  return 0.7152 * g;
+}
+function getBlueLuminance(b) {
+  return 0.0722 * b;
+}
+function getLuminance(r, g, b) {
+  let luminance =
+    getRedLuminance(r) +
+    getGreenLuminance(g)
+    + getBlueLuminance(b);
+  return luminance;
+}
+
+const bayerMatrices = [
+  // 1*1  2^0
+  [0],
+
+  // 2*2  2^1
+  [0, 2,
+    3, 1],
+
+  // 4*4  2^2
+  [0, 8, 2, 10,
+    12, 4, 14, 6,
+    3, 11, 1, 9,
+    15, 7, 13, 5],
+
+  // 8*8  2^3
+  [0, 32, 8, 40, 2, 34, 10, 42,
+    48, 16, 56, 24, 50, 18, 58, 26,
+    12, 44, 4, 36, 14, 46, 6, 38,
+    60, 28, 52, 20, 62, 30, 54, 22,
+    3, 35, 11, 43, 1, 33, 9, 41,
+    51, 19, 59, 27, 49, 17, 57, 25,
+    15, 47, 7, 39, 13, 45, 5, 37,
+    63, 31, 55, 23, 61, 29, 53, 21]
+];
